646dc465f0f9deacf00fe339cea67272ed726cc2
Chandan Kumar <chandu.92b@gmail.com>
2024-10-18 00:50:38 +0530

    New repo testing created

---
+++ src/Tasks/Gradecalculator.java
@@ -0,0 +1,30 @@
+package Tasks;
+import java.util.Scanner;
+public class Gradecalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the score(0-100):");
+        int score = scanner.nextInt();
+        char grade;
+        if(score>90 && score<=100){
+            grade = 'A';
+        } else if (score>80 && score<=89) {
+            grade = 'B';
+        }
+        else if (score>70 && score<=79) {
+            grade = 'C';
+        }
+        else if (score>60 && score<=69) {
+            grade = 'D';
+        }
+        else if (score>0 && score<=59) {
+            grade = 'F';
+        }
+        else {
+            System.out.println("Enter Valid score between 0 to 100");
+            return;
+        }
+
+        System.out.println("Grade is :"+ grade);
+    }
+}
