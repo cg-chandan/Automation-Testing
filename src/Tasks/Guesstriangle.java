b987a0fc7eefa72a5b87c80c8eef737cfdfe872b
Chandan Kumar <chandu.92b@gmail.com>
2024-10-30 23:53:17 +0530

    updated 31st Oct code

--- src/Tasks/Guesstriangle.java
+++ src/Tasks/Guesstriangle.java
@@ -5,10 +5,13 @@ public class Guesstriangle {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Double side1,side2,side3;
-        side = scanner.nextDouble();
+        Double side1, side2, side3;
         System.out.println("Enter the lengths of the three sides of the triangle:");
+        side1 = scanner.nextDouble();
+        side2 = scanner.nextDouble();
+        side3 = scanner.nextDouble();
 
+        if (side1 == side2 && side2 == side3) {
             System.out.println("The triangle is equilateral.");
         } else if (side1 == side2 || side1 == side3 || side2 == side3) {
             System.out.println("The triangle is isosceles.");
@@ -16,6 +19,6 @@ public class Guesstriangle {
             System.out.println("The triangle is scalene.");
         }
 
-        scanner.close();
     }
-}
+    }
+
