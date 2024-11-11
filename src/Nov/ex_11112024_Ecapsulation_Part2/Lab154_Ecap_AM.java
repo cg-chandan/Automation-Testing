0bd8d1e592a353d4ef9a72f0f7a69ab20eea7357
Chandan Kumar <chandu.92b@gmail.com>
2024-11-11 23:10:59 +0530

    updated 11th Nov Ecap files

---
+++ src/Nov/ex_11112024_Ecapsulation_Part2/Lab154_Ecap_AM.java
@@ -0,0 +1,19 @@
+package Nov.ex_11112024_Ecapsulation_Part2;
+
+import Nov.ex_11112024_Ecapsulation_Part2.REAL.BaseClass;
+import Nov.ex_11112024_Ecapsulation_Part2.REAL.TestCase1;
+
+public class Lab154_Ecap_AM {
+
+    //     // public, private, protected, default
+
+    public static void main(String[] args) {
+        TestCase1 tc1 = new TestCase1(); // Dynamic DisPatch
+        tc1.setBrowser("chrome",true);
+        tc1.startTestCase();
+
+
+    }
+
+
+}
