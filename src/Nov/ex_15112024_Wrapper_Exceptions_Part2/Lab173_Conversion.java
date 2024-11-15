95bd9db61e6c0edf139a10543c1e3269bfdf2653
Chandan Kumar <chandu.92b@gmail.com>
2024-11-15 22:52:04 +0530

    updated 15th Nov

---
+++ src/Nov/ex_15112024_Wrapper_Exceptions_Part2/Lab173_Conversion.java
@@ -0,0 +1,22 @@
+package Nov.ex_15112024_Wrapper_Exceptions_Part2;
+
+public class Lab173_Conversion {
+    public static void main(String[] args) {
+
+        String num = "10";
+        // String -> Wrapper(Integer) -> Primitive Int
+        Integer a = Integer.valueOf(num);
+        int a2 = a;// unboxing
+
+        // String to Primitive
+        int a3  = Integer.parseInt(num);
+
+        Long l = 10l;
+        Short s= 23;
+        Boolean bb = true;
+
+
+
+
+    }
+}
