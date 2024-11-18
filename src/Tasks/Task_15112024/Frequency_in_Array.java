23311780cf11ebb764ae6298c31b98c3609ed314
Chandan Kumar <chandu.92b@gmail.com>
2024-11-18 21:47:48 +0530

    task 15th Nov

--- src/Tasks/Task_15112024/Frequency_in_Array.java
+++ src/Tasks/Task_15112024/Frequency_in_Array.java
@@ -3,6 +3,12 @@ package Tasks.Task_15112024;
 public class Frequency_in_Array {
     public static void main(String[] args) {
         int[] arr = {1,2,2,3,4,5,5,6};
+        int max  = 0;
+        for(int num:arr){
+            if(num > max){
+                max = num;
+            }
 
+        }
     }
 }
