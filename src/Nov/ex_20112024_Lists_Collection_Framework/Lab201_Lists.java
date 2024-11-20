bca247cdfd36947ac3c593035fccab7f8b01a141
Chandan Kumar <chandu.92b@gmail.com>
2024-11-20 23:03:59 +0530

    20th Nov

---
+++ src/Nov/ex_20112024_Lists_Collection_Framework/Lab201_Lists.java
@@ -0,0 +1,20 @@
+package Nov.ex_20112024_Lists_Collection_Framework;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Lab201_Lists {
+    public static void main(String[] args) {
+        List mylist = new ArrayList(); // Arrays Format - Continuous
+        List mylist2 = new LinkedList(); // Doubly Linked List.
+        // How they are stored in the memory
+
+
+        List fruits = List.of("Orange","Apple");
+        fruits.add("WaterMelon"); // java.lang.UnsupportedOperationException
+
+
+
+    }
+}
