a6138aee3ba3e1948d88295f7f70c2b13b20dc8c
Chandan Kumar <chandu.92b@gmail.com>
2024-11-21 21:26:34 +0530

    21st Nov

---
+++ src/Nov/ex_21112024_List_Set_Collection_Framework/Lab212_SET.java
@@ -0,0 +1,26 @@
+package Nov.ex_21112024_List_Set_Collection_Framework;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Lab212_SET {
+    public static void main(String[] args) {
+        Set hs = new HashSet();
+
+        Set lhs = new LinkedHashSet();
+
+        Set ts = new TreeSet();
+
+
+        hs.add("Pramod");
+        hs.add("Pramod");
+        hs.add("pramod");
+        hs.add("dramod");
+        System.out.println(hs);
+
+
+
+    }
+}
---
+++ src/Nov/ex_21112024_List_Set_Collection_Framework/Lab215_Vector.java
@@ -0,0 +1,31 @@
+package Nov.ex_21112024_List_Set_Collection_Framework;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Lab215_Vector {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+        vector.add("Apple");
+        vector.add("Banana");
+        vector.add("Cherry");
+        vector.add("Date");
+
+        for (String o: vector){
+            System.out.println(o);
+        }
+
+        // Iterator? - For legacy Class I can't use
+        // Enumeration
+
+        System.out.println(" --- ");
+
+        Enumeration<String> enumeration = vector.elements();
+        while (enumeration.hasMoreElements()){
+            System.out.println(enumeration.nextElement());
+        }
+
+
+
+    }
+}
