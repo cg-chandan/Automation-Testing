4b9c531d04100bedb17fc6d2213c381f34ca49d3
Chandan Kumar <chandu.92b@gmail.com>
2024-11-23 00:03:23 +0530

    Task 20th Nov

--- src/Tasks/Task_20112024/Array_reverse.java
+++ src/Tasks/Task_20112024/Array_reverse.java
@@ -4,6 +4,10 @@ public class Array_reverse {
     public static void main(String[] args) {
 
         int[] arr = {1,3,5,6,7};
+        System.out.print("Reversed array:");
+        for (int i = arr.length-1; i >=0 ; i--) {
+            System.out.print(arr[i]+ " ");
+        }
 
     }
 }
