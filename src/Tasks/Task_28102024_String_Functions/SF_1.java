8226be141d80df6b9479747cf6f235e0e3f4d068
Chandan Kumar <chandu.92b@gmail.com>
2024-11-28 23:36:58 +0530

    String function Oct 28th

--- src/Tasks/Task_28102024_String_Functions/SF_1.java
+++ src/Tasks/Task_28102024_String_Functions/SF_1.java
@@ -1,4 +1,7 @@
 package Tasks.Task_28102024_String_Functions;
 
 public class SF_1 {
+    public static void main(String[] args) {
+        System.out.println("Print the reverse of string");
+    }
 }
