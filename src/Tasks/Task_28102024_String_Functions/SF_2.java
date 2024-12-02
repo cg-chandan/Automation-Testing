71728342fdf57a9e66ac22f3065e5dda3788a548
Chandan Kumar <chandu.92b@gmail.com>
2024-12-02 23:33:53 +0530

    String function Oct 28th

--- src/Tasks/Task_28102024_String_Functions/SF_2.java
+++ src/Tasks/Task_28102024_String_Functions/SF_2.java
@@ -2,8 +2,6 @@ package Tasks.Task_28102024_String_Functions;
 
 public class SF_2 {
     public static void main(String[] args) {
-        System.out.println("Enappa nindu golu?!");
-        System.out.println("hhhyyy");
-        System.out.println("en madtyo madu...........");
+
     }
 }
