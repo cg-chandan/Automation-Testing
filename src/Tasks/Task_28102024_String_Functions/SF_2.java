453c9ec5e630857cd648fb6f730a8c8d58fb289c
Chandan Kumar <chandu.92b@gmail.com>
2024-12-06 22:43:14 +0530

    String function Oct 28th

--- src/Tasks/Task_28102024_String_Functions/SF_2.java
+++ src/Tasks/Task_28102024_String_Functions/SF_2.java
@@ -4,5 +4,6 @@ public class SF_2 {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("dfghfjgdffx");
+        System.out.println("akeeqoqoq");
     }
 }
