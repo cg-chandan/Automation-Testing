8fec1164e83b983fe49c41fee78d721500bc6e62
Chandan Kumar <chandu.92b@gmail.com>
2024-12-14 23:25:03 +0530

    String function Oct 28th

--- src/Tasks/Task_28102024_String_Functions/SF_2.java
+++ src/Tasks/Task_28102024_String_Functions/SF_2.java
@@ -3,5 +3,6 @@ package Tasks.Task_28102024_String_Functions;
 public class SF_2 {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println("ppoiii");
     }
 }
